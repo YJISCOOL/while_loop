42c73e12d4923c80919f87d2fc553d5c0f5a1f86
YJISCOOL <star000321@gmail.com>
2020-10-17 21:38:22 +0800

    while_loop

--- while_loop-reset_password.py
+++ while_loop-reset_password.py
@@ -1,11 +1,15 @@
-chance = 0
+chance = 3
 
-while chance <= 2:
+while chance > 0:
 	input_password = input('enter ur password: ')
-	#print (input_password)
+	chance -= 1
 	if input_password == 'a123456':
 		print ('登入成功！')
 		break
 	else:
-		print ('密碼錯誤！還有'+ str(2-chance) +'次機會。')
-	chance += 1
+		if chance > 0:
+			print ('密碼錯誤！還有'+ str(chance) +'次機會。')
+		else:
+			print ('你忘記就去按忘記密碼啊，很難嗎')
+	
+	
