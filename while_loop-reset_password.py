de098d5abf7adda6a607ab1bc45af1e8a9f0a9d8
YJISCOOL <star000321@gmail.com>
2020-10-17 20:47:47 +0800

    while_loop

---
+++ while_loop-reset_password.py
@@ -0,0 +1,11 @@
+chance = 0
+
+while chance <= 2:
+	input_password = input('enter ur password: ')
+	print (input_password)
+	if input_password == 'a123456':
+		print ('登入成功！')
+		break
+	else:
+		print ('密碼錯誤！還有'+ str(2-chance) +'次機會。')
+	chance += 1
